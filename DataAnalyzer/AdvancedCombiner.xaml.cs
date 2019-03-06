7d518598d8c0f2029a9900a66a1fd639e2124be1
Netanel <netanel.elimelech@gmail.com>
2019-03-06 09:24:43 +0200

    Added: Force including of spicified numbers (up to five numbers)

--- DataAnalyzer/AdvancedCombiner.xaml.cs
+++ DataAnalyzer/AdvancedCombiner.xaml.cs
@@ -74,43 +74,108 @@ namespace DataAnalyzer
 
             //Display
             int count = 0;
+
+            List<string> includedList = IncludedNumbers();
+
             for (int i = 0; i < tempCombinationsSixArrayInt.Length; i++)
             {
-                string combination = "";
+                string combination = " ";
                 for (int j = 0; j < tempCombinationsSixArrayInt[i].Length; j++)
                 {
                     combination += $"{tempCombinationsSixArrayInt[i][j].ToString()} ";
                 }
 
-                //For: 1 2 8 10 11 12 14 18 22 25 26 27 29 31 33 35
-
-                if (combination.Contains("11")
-                && !combination.Contains("11 25")
-                && !combination.Contains("11 26")
-                && !combination.Contains("11 27")
-                && !combination.Contains("11 29")
-                && !combination.Contains("11 31")
-                && !combination.Contains("11 33")
-                && !combination.Contains("11 35")
-                && !combination.Contains("12 25")
-                && !combination.Contains("12 26")
-                && !combination.Contains("12 27")
-                && !combination.Contains("12 29")
-                && !combination.Contains("12 31")
-                && !combination.Contains("12 33")
-                && !combination.Contains("12 35")
-                && !combination.Contains("14 27")
-                && !combination.Contains("14 29")
-                && !combination.Contains("14 31")
-                && !combination.Contains("14 33")
-                && !combination.Contains("14 35")
-                && !(combination.Contains("10") && combination.Contains("11") && combination.Contains("12"))
-                && !(combination.Contains("25") && combination.Contains("26") && combination.Contains("27"))
-                )
+                //For: 1 2 8 10 11 12 14 18 22 25 26 27 29 30 31 33 35
+                if (includedList.Count != 0)
                 {
-                    count++;
-                    outputTextBoxADV.AppendText($"{combination}\n");
+                    foreach (var item in includedList)
+                    {
+                        if (combination.Contains(item))
+                        {
+                            outputTextBoxADV.AppendText($"{combination}\n");
+                            count++;
+                        }
+                    }
                 }
+
+                else
+                {
+                    outputTextBoxADV.AppendText($"{combination}\n");
+                    count++;
+                }
+
+                //if (combination.Contains("18")
+                //&& !combination.Contains("1 14")
+                ////&& !combination.Contains("1 18")
+                //&& !combination.Contains("1 22")
+                //&& !combination.Contains("1 25")
+                //&& !combination.Contains("1 26")
+                //&& !combination.Contains("1 27")
+                //&& !combination.Contains("1 29")
+                //&& !combination.Contains("1 30")
+                //&& !combination.Contains("1 31")
+                //&& !combination.Contains("1 33")
+                //&& !combination.Contains("1 35")
+                //&& !combination.Contains("2 14")
+                ////&& !combination.Contains("2 18")
+                //&& !combination.Contains("2 22")
+                //&& !combination.Contains("2 25")
+                //&& !combination.Contains("2 26")
+                //&& !combination.Contains("2 27")
+                //&& !combination.Contains("2 29")
+                //&& !combination.Contains("2 30")
+                //&& !combination.Contains("2 31")
+                //&& !combination.Contains("2 33")
+                //&& !combination.Contains("2 35")
+                ////&& !combination.Contains("8 22")
+                ////&& !combination.Contains("8 25")
+                ////&& !combination.Contains("8 26")
+                ////&& !combination.Contains("8 27")
+                ////&& !combination.Contains("8 29")
+                ////&& !combination.Contains("8 30")
+                ////&& !combination.Contains("8 31")
+                ////&& !combination.Contains("8 33")
+                ////&& !combination.Contains("8 35")
+                //&& !combination.Contains("10 22")
+                //&& !combination.Contains("10 25")
+                //&& !combination.Contains("10 26")
+                //&& !combination.Contains("10 27")
+                //&& !combination.Contains("10 29")
+                //&& !combination.Contains("10 30")
+                //&& !combination.Contains("10 31")
+                //&& !combination.Contains("10 33")
+                //&& !combination.Contains("10 35")
+                //&& !combination.Contains("11 25")
+                //&& !combination.Contains("11 26")
+                //&& !combination.Contains("11 27")
+                //&& !combination.Contains("11 29")
+                //&& !combination.Contains("11 30")
+                //&& !combination.Contains("11 31")
+                //&& !combination.Contains("11 33")
+                //&& !combination.Contains("11 35")
+                //&& !combination.Contains("12 25")
+                //&& !combination.Contains("12 26")
+                //&& !combination.Contains("12 27")
+                //&& !combination.Contains("12 29")
+                //&& !combination.Contains("12 30")
+                //&& !combination.Contains("12 31")
+                //&& !combination.Contains("12 33")
+                //&& !combination.Contains("12 35")
+                //&& !combination.Contains("14 26")
+                //&& !combination.Contains("14 27")
+                //&& !combination.Contains("14 29")
+                //&& !combination.Contains("14 30")
+                //&& !combination.Contains("14 31")
+                //&& !combination.Contains("14 33")
+                //&& !combination.Contains("14 35")
+                //&& !(combination.Contains("10") && combination.Contains("11") && combination.Contains("12"))
+                //&& !(combination.Contains("25") && combination.Contains("26") && combination.Contains("27"))
+                //&& !(combination.Contains("29") && combination.Contains("30") && combination.Contains("31"))
+                //)
+                //{
+                //    outputTextBoxADV.AppendText($"{combination}\n");
+                //    count++;
+                //}
             }
             CombinationsCount.Content = $"Count: {count}";
             count = 0;
@@ -204,6 +269,29 @@ namespace DataAnalyzer
             }
 
             return finalCombinationsArrayFiltered;
+        }
+
+        List<string> IncludedNumbers()
+        {
+            List<string> includedList = new List<string>();
+            if (int.TryParse(Include1TextBox.Text, out int parsedValue) && parsedValue >= 1 && parsedValue <= 37)
+                includedList.Add(parsedValue.ToString());
+            if (int.TryParse(Include2TextBox.Text, out parsedValue) && parsedValue >= 1 && parsedValue <= 37)
+                includedList.Add(parsedValue.ToString());
+            if (int.TryParse(Include3TextBox.Text, out parsedValue) && parsedValue >= 1 && parsedValue <= 37)
+                includedList.Add(parsedValue.ToString());
+            if (int.TryParse(Include4TextBox.Text, out parsedValue) && parsedValue >= 1 && parsedValue <= 37)
+                includedList.Add(parsedValue.ToString());
+            if (int.TryParse(Include5TextBox.Text, out parsedValue) && parsedValue >= 1 && parsedValue <= 37)
+                includedList.Add(parsedValue.ToString());
+
+            for (int i = 0; i < includedList.Count; i++)
+            {
+                includedList[i] = includedList[i].Insert(0, " ");
+                includedList[i] = includedList[i].Insert(2, " ");
+            }
+
+            return includedList;
         }
 
         private void ClearCombinationsButtonADV_Click(object sender, RoutedEventArgs e)
